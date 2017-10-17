c6071093c35c2a83a683fe55788946ae99b38256
David Heimann <davidheimann@gmail.com>
2017-10-17 16:39:17 -0400

    Update CC Object to limit fields, use custom exception and rules

--- contacts/api.py
+++ contacts/api.py
@@ -8,6 +8,8 @@ This module implements the Contacts 📕 API.
 
 import vobject
 
+from .exceptions import ContactCreationException
+from .rules import ALLOWED_FIELDS
 
 class ContactCard(object):
     """
@@ -22,7 +24,29 @@ class ContactCard(object):
     :param twitter: Twitter Username (ex: @david_heimann)
     """
 
+    _allowed_fields = ALLOWED_FIELDS + ['_card', '_card_field']
     _card = None
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         self._card = vobject.vCard()
+
+        # all those keys will be initialized as class attributes
+        allowed_keys = set(ALLOWED_FIELDS)
+        # initialize all allowed keys to false
+        self.__dict__.update((key, False) for key in allowed_keys)
+        # and update the given keys by their given values
+        self.__dict__.update((key, value) for key, value in kwargs.items() if key in allowed_keys)
+
+        if not self.name:
+            raise ContactCreationException(
+                "A Contact Card must have a name associated with it."
+            )
+
+    def __setattr__(self, attribute, value):
+        if not attribute in set(self._allowed_fields):
+            print("{0} is not a valid attribute of a Contact Card.\nValid attributes are: {1}".format(
+                attribute,
+                ALLOWED_FIELDS
+            ))
+        else:
+            self.__dict__[attribute] = value
