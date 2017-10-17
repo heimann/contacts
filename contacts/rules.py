eb9eaaf99d48f01c556576efc60e260fa0f8a7c8
David Heimann <davidheimann@gmail.com>
2017-10-17 16:38:51 -0400

    Add rules file for configuration of attributes

---
+++ contacts/rules.py
@@ -0,0 +1,18 @@
+"""
+contacts.rules
+~~~~~~~~~~~~
+This module sets rules for Contacts 📕.
+:copyright: (c) 2017 by David Heimann.
+:license: MIT, see LICENSE for more details.
+"""
+
+
+ALLOWED_FIELDS = [
+    'name',
+    'first_name',
+    'last_name',
+    'phone_number',
+    'photo',
+    'email',
+    'twitter'
+]
