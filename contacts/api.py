90fd06d8dfad0222c3f7554f2fd14ff7effa3e1e
David Heimann <davidheimann@gmail.com>
2017-10-17 16:16:25 -0400

    Add api file with barebones CC Object

---
+++ contacts/api.py
@@ -0,0 +1,28 @@
+"""
+contacts.api
+~~~~~~~~~~~~
+This module implements the Contacts 📕 API.
+:copyright: (c) 2012 by David Heimann.
+:license: MIT, see LICENSE for more details.
+"""
+
+import vobject
+
+
+class ContactCard(object):
+    """
+    A :class:`Contact Card <ContactCard>` object.
+
+    :param name: Full Name (required).
+    :param first_name: First Name.
+    :param last_name: Last Name.
+    :param photo: fileobject of photo.
+    :param email: E-Mail address.
+    :param website: URL.
+    :param twitter: Twitter Username (ex: @david_heimann)
+    """
+
+    _card = None
+
+    def __init__(self):
+        self._card = vobject.vCard()
