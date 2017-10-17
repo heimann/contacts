7cfc023942e7d6ec4a7d2c4adbb48f86c304783d
David Heimann <davidheimann@gmail.com>
2017-10-17 16:38:40 -0400

    Add custom exception file

---
+++ contacts/exceptions.py
@@ -0,0 +1,10 @@
+"""
+contacts.exceptions
+~~~~~~~~~~~~
+Exceptions ⚠️ for Contacts 📕.
+:copyright: (c) 2017 by David Heimann.
+:license: MIT, see LICENSE for more details.
+"""
+
+class ContactCreationException(Exception):
+    """An Exception occured when creating the Contact Card."""
