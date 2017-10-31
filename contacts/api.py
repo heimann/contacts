18e30cbf25fcd0b31f4359a6d84609ca13e972e0
heimann <davidheimann@gmail.com>
2017-10-31 14:46:22 -0400

    Remove unnecessary comments

--- contacts/api.py
+++ contacts/api.py
@@ -48,11 +48,8 @@ class ContactCard(object):
         """
         self._card = vobject.vCard()
 
-        # all those keys will be initialized as class attributes
         allowed_keys = set(ALLOWED_FIELDS)
-        # initialize all allowed keys to false
         self.__dict__.update((key, False) for key in allowed_keys)
-        # and update the given keys by their given values
         self.__dict__.update((key, value) for key, value in kwargs.items() if key in allowed_keys)
 
 
